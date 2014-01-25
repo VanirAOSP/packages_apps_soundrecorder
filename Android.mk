dd6102140ed85a6b8c2e813b2b848e657c17c195
DHOMD <DHO@dho.im>
2014-01-25 11:27:32 -0500

    Build to Data
    Potentially make the ROM lighter and more customizable.  Likely other apps can benefit from this
    
    Change-Id: Ie13acf17d5be7db537bf19df02f5eb4389ab88e0

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
