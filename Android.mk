b287807b75858fd0b44058727a3a25bf29ee3ae8
nuclearmistake <nuclearmistake@gmail.com>
2014-02-07 00:33:18 -0500

    Revert "Build to Data"
    We have a system app remover... not a data app remover.... wwwhhhyyyyy??
    
    This reverts commit dd6102140ed85a6b8c2e813b2b848e657c17c195.

--- Android.mk
+++ Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
